e24397fd34fd650b452d451f85bfe2a94d25a6c3
Mikhail Zagoruiko <zagoruiko.mikhail@gmail.com>
2026-06-30 06:07:45 +1000

    [FRONTEND] Improved styling

--- frontend/src/app/authentication/authentication.tsx
+++ frontend/src/app/authentication/authentication.tsx
@@ -1,12 +1,19 @@
 import styles from './authentication.module.css'
+import {FcGoogle} from "react-icons/fc";
+import {IoMdExit} from "react-icons/io";
+import {useNavigate} from "react-router";
 
 export default function Authentication() {
+    const navigate = useNavigate();
+
     return (
         <div className={styles.Authentication}>
             <div className={styles.AuthenticationInner}>
                 <h2>Sign in or create your account</h2>
 
-                <button>Sign in with Google</button>
+                <button><FcGoogle className={styles.Icon}/>Sign in with Google</button>
+
+                <button onClick={() => navigate("/")}><IoMdExit className={styles.Icon}/> Return back</button>
             </div>
         </div>
     )
